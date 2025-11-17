e1d016fb3d0f24501d5e1d4a67f43cc71fa36ceb
Kanyanat Uetrakulchai <kanyanat.uet@student.mahidol.ac.th>
2025-11-17 20:43:14 +0700

    Fix Error Krub

--- src/main/java/com/devsop/project/apartmentinvoice/config/OpenTelemetryConfig.java
+++ src/main/java/com/devsop/project/apartmentinvoice/config/OpenTelemetryConfig.java
@@ -1,20 +1,18 @@
 package com.devsop.project.apartmentinvoice.config;
 
-import static io.opentelemetry.semconv.ResourceAttributes.DEPLOYMENT_ENVIRONMENT;
-import static io.opentelemetry.semconv.ResourceAttributes.SERVICE_NAME;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporterBuilder;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
@@ -26,10 +24,10 @@ public class OpenTelemetryConfig {
 
   @Bean(destroyMethod = "shutdown")
   public OtlpGrpcSpanExporter otlpGrpcSpanExporter(
-    @Value("${observability.otel.endpoint:http://localhost:4318/v1/traces}") String endpoint,
+    @Value("${observability.otel.endpoint:http://otel-collector-svc.doomed-apt.svc.cluster.local:4318/v1/traces}") String endpoint,
     @Value("${observability.otel.headers:}") String headers
   ) {
-    OtlpGrpcSpanExporter.Builder builder = OtlpGrpcSpanExporter.builder()
+    OtlpGrpcSpanExporterBuilder builder = OtlpGrpcSpanExporter.builder()
       .setEndpoint(endpoint);
 
     if (headers != null && !headers.isBlank()) {
@@ -55,8 +53,8 @@ public class OpenTelemetryConfig {
 
     Resource resource = Resource.getDefault().merge(Resource.create(
       Attributes.builder()
-        .put(SERVICE_NAME, appName)
-        .put(DEPLOYMENT_ENVIRONMENT, profile)
+        .put("service.name", appName)
+        .put("deployment.environment", profile)
         .build()
     ));
 
@@ -76,7 +74,7 @@ public class OpenTelemetryConfig {
           W3CTraceContextPropagator.getInstance(),
           W3CBaggagePropagator.getInstance()
         )
-      )))
+      ))
       .build();
   }
 
