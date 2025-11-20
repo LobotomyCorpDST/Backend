a49e7c3cdd84e67fcc2fcc67913d87c34474ea18
Drillad <pencygt@gmail.com>
2025-11-20 15:22:06 +0700

    delete button for maintenance edit modal

--- src/main/java/com/devsop/project/apartmentinvoice/controller/MaintenanceController.java
+++ src/main/java/com/devsop/project/apartmentinvoice/controller/MaintenanceController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -204,6 +206,16 @@ public MaintenanceResponse update(
   return toDto(m);
   }
 
+  // ---------- Delete ----------
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable Long id) {
+    Maintenance m = maintenanceRepo.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "Maintenance id " + id + " not found"));
+
+    maintenanceRepo.delete(m);
+  }
 
   // ---------- mapper ----------
   private MaintenanceResponse toDto(Maintenance m) {
